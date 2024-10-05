016c52f34f98f0741c28daee9a6158abac4bdf91
Arthri <41360489+a@users.noreply.github.com>
2024-10-05 13:21:24 +0000

    Project files

---
+++ src/Savepatch/Locator.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Versioning;
+using System.Text.RegularExpressions;
+
+internal static partial class Locator
+{
+    private static bool LocateSteam(
+        [NotNullWhen(true)] out string? path
+    )
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            if (LocateSteamUsingRegistry(out path))
+            {
+                return true;
+            }
+        }
+
+        path = null;
+        return false;
+    }
+
+    [SupportedOSPlatform("windows")]
+    private static bool LocateSteamUsingRegistry(
+        [NotNullWhen(true)] out string? path
+    )
+    {
+        object? pathKey = Registry.CurrentUser.OpenSubKey("""SOFTWARE\Valve\Steam""")?.GetValue("SteamPath");
+        pathKey ??= (Environment.Is64BitOperatingSystem
+            ? Registry.LocalMachine.OpenSubKey("""SOFTWARE\Wow6432Node\Valve\Steam""")
+            : Registry.LocalMachine.OpenSubKey("""SOFTWARE\Valve\Steam"""))
+            ?.GetValue("InstallPath")
+            ;
+        if (pathKey is not (null or string))
+        {
+            Console.WriteLine($"Unknown key type. ({pathKey.GetType().Name})");
+            path = null;
+            return false;
+        }
+        else if (pathKey is string pathString)
+        {
+            path = pathString;
+            return true;
+        }
+
+        path = null;
+        return false;
+    }
+
+    internal static bool LocateTerraria(
+        [NotNullWhen(true)] out string? path
+    )
+    {
+        if (LocateSteam(out string? steamPath))
+        {
+            var steamappsPath = Path.Combine(steamPath, "steamapps");
+            var appmanifestPath = Path.Combine(steamappsPath, "appmanifest_105600.acf");
+            if (File.Exists(appmanifestPath))
+            {
+                var appmanifest = File.ReadAllText(appmanifestPath);
+                var match = InstalldirMatcher().Match(appmanifest);
+                if (match.Success)
+                {
+                    var installDir = Path.Combine(steamappsPath, "common", match.Value);
+                    var terrariaPath = Path.Combine(installDir, "Terraria.exe");
+                    if (File.Exists(terrariaPath))
+                    {
+                        path = terrariaPath;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        if (File.Exists("Terraria.exe"))
+        {
+            path = Path.GetFullPath("Terraria.exe");
+            return true;
+        }
+
+        path = null;
+        return false;
+    }
+
+    [GeneratedRegex("(?<=\t\"installdir\"\t\t\")[^\"]+(?=\")")]
+    private static partial Regex InstalldirMatcher();
+}
---
+++ src/Savepatch/Program.cs
@@ -0,0 +1,85 @@
+﻿using AsmResolver.DotNet;
+using AsmResolver.PE.DotNet.Cil;
+
+if (!Locator.LocateTerraria(out string? terrariaPath))
+{
+    Console.WriteLine("Unable to locate Terraria.");
+    goto exit;
+}
+
+try
+{
+    File.Copy(
+        terrariaPath,
+        Path.Combine(
+            Path.GetDirectoryName(terrariaPath) ?? "",
+            $"{Path.GetFileNameWithoutExtension(terrariaPath)}-original{Path.GetExtension(terrariaPath)}"
+        )
+    );
+}
+catch (IOException)
+{
+    Console.WriteLine("Unable to write to Terraria-original.exe, because of insufficient permissions, it already exists, or some other reason.");
+    goto exit;
+}
+
+var assembly = AssemblyDefinition.FromFile(terrariaPath);
+var module = assembly.ManifestModule;
+if (module is null)
+{
+    Console.WriteLine("Unable to find module.");
+    goto exit;
+}
+
+var type = module.TopLevelTypes.FirstOrDefault(t => t.FullName == "Terraria.Player");
+if (type is null)
+{
+    Console.WriteLine("Unsupported client(Player type could not be found).");
+    goto exit;
+}
+
+var method = type.Methods.FirstOrDefault(m => m.Name == "InternalSavePlayerFile");
+if (method is null)
+{
+    Console.WriteLine("Unsupported client(Save method could not be found).");
+    goto exit;
+}
+
+var methodBody = method.CilMethodBody;
+if (methodBody is null)
+{
+    Console.WriteLine("Unsupported client(Save method is not valid).");
+    goto exit;
+}
+
+var instructions = methodBody.Instructions;
+if (instructions is
+[
+    { OpCode.Code: CilCode.Ldarg_0 },
+    {
+        OpCode.Code: CilCode.Callvirt,
+        Operand: MethodDefinition
+        {
+            FullName: "System.Boolean Terraria.IO.PlayerFileData::get_ServerSideCharacter()",
+        },
+    },
+    { OpCode.Code: CilCode.Brfalse or CilCode.Brfalse_S },
+    { OpCode.Code: CilCode.Ret },
+    ..
+])
+{
+    instructions.RemoveRange(0, 4);
+
+    assembly.Write(terrariaPath);
+
+    Console.WriteLine("Successfully patched.");
+
+    goto exit;
+}
+
+Console.WriteLine("Unsupported client(Save method contains unexpected sequence).");
+goto exit;
+
+exit:
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey(true);
